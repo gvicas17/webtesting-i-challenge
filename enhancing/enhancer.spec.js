6c2bee3d8189b9c2e3b0288eadebb3e984cd9563
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-27 16:17:05 -0500

    met mvp

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,3 +6,27 @@ test('sanity', () => {
     expect(3).not.toBe(4)
     expect(3).toBeGreaterThan(2)
 })
+
+describe('enhancer', () => {
+    const item = {
+        name: 'laptop',
+        durability: 90, 
+        enhancement: 18
+      }
+    it('exists', () => {
+        expect(enhancer).toBeDefined
+    })
+    it('repairs item', () => {
+        expect(enhancer.repair(item)).toHaveProperty('durability')
+        expect(item.durability).toBe(100)
+    })
+    it('enhances item', () => {
+        expect(enhancer.success(item)).toHaveProperty('enhancement')
+        expect(item.enhancement).toBe(19)
+    })
+    it('fails', () =>{
+        expect(enhancer.fail(item)).toHaveProperty('durability')
+        expect(item.enhancement).toBe(18)
+        expect(item.durability).toBe(90)
+    } )
+})
